56176e19202abb0fdc06fa03bdae4be0bead699f
Manikandan <43571823+infinire@users.noreply.github.com>
2024-08-19 15:20:38 +0530

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+let MONGO_URI= 'mongodb+srv://expressjsdemo:Mpa54EQRqCMiaIs3@subscriptiondemo.zmrzkxg.mongodb.net/Subscription?retryWrites=true&w=majority&appName=SubscriptionDemo';
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
+    await mongoose.connect(MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true});
     console.log('MongoDB connected');
